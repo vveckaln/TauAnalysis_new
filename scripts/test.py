b88c62ee179373311d5a2dab987331d82b95bbe8
vveckaln <viesturs.veckalns@cern.ch>
2015-10-08 14:21:23 +0100

    8 okt

--- scripts/test.py
+++ scripts/test.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python
-import subprocess
-import os
-PROXYDIR="/lustre/ncg.ingrid.pt/cmslocal/viesturs/RunII/FARM/inputs"
-cmd='mkdir -p ' + PROXYDIR + '; voms-proxy-init --voms cms             -valid 720:00 --out ' + PROXYDIR + '/x509_proxy'
-print cmd
-#cr = subprocess.check_output(cmd)
-#os.system(cmd)
-out=subprocess.check_output(cmd, shell=True)
-#out,err = cr.communicate()
-print "output\n", out
-if out.find("Hello") > -1:
-    print "hey"
+            #sys.stdout.write(out)
+        #    print "from here"
+            #sys.stdout.flush()
+            
+    """
+            totalerr=''
+    while True:
+        
+        err = p.stderr.read(1)
+        totalerr += err
+        if err == '' and p.poll() != None:
+            break
+        if err != '':
+            sys.stderr.write(err)
+       #     print "from here"
+            sys.stderr.flush()
+    if totalerr.find("No credentials found!") == -1:
+        break
+       
+#print "output\n", out
+#if out.find("Hello") > -1:
+#    print "hey"
+"""
