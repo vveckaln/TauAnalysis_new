d0929f2da32576cfd592fb2842be71a9998dfde6
vveckaln <viesturs.veckalns@cern.ch>
2015-10-08 14:42:00 +0100

    8 okt 2

--- scripts/test.py
+++ scripts/test.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python
+import subprocess
+import os
+import sys
+PROXYDIR="/lustre/ncg.ingrid.pt/cmslocal/viesturs/RunII/FARM/inputs"
+cmd='mkdir -p ' + PROXYDIR + '; voms-proxy-init --voms cms             -valid 720:00 --out ' + PROXYDIR + '/x509_proxy'
+print cmd
+#cr = subprocess.check_output(cmd)
+#os.system(cmd)
+while True:
+    p=subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+#out,err=proc.communicate()
+    totalout=''
+    
+    while True:
+        out = p.stdout.readline()
+        if out == '' and p.poll() != None:
+            break
+        if out:
+            self.logger.info(out.strip())
             #sys.stdout.write(out)
         #    print "from here"
             #sys.stdout.flush()
