84477625f305f52c48c5a32528f207adda2e25bf
vveckaln <viesturs.veckalns@cern.ch>
2015-11-26 18:11:34 +0000

    succesfull VID test

--- bin/VID_test.cc
+++ bin/VID_test.cc
@@ -5,6 +5,10 @@
 #include "DataFormats/FWLite/interface/Event.h"
 #include "DataFormats/PatCandidates/interface/Electron.h"
 #include "EgammaWork/ElectronNtupler/plugins/ElectronNtuplerVIDDemo.cc"
+
+#include "DataFormats/FWLite/interface/InputSource.h"
+#include "DataFormats/FWLite/interface/OutputFiles.h"
+#include "RecoEgamma/ElectronIdentification/interface/VersionedGsfElectronSelector.h"
 #include "TFile.h"
 #include "TSystem.h"
 int main(int argc, char* argv[])
@@ -12,7 +16,28 @@ int main(int argc, char* argv[])
   gSystem->Load( "libFWCoreFWLite.so" );
   gSystem->Load( "libCERN_RTUTauAnalysis.so" );
   AutoLibraryLoader::enable();
-  const edm::ParameterSet &iConfig = edm::readPSetsFrom(argv[1]) -> getParameter<ElectronNtuplerVIDDemo>("process.ntupler");
+
+  if( !edm::readPSetsFrom(argv[1])->existsAs<edm::ParameterSet>("process") ){
+    std::cout << " ERROR: ParametersSet 'process' is missing in your configuration file" << std::endl; exit(0);
+  }
+  // get the python configuration
+  const edm::ParameterSet& process = edm::readPSetsFrom(argv[1])->getParameter<edm::ParameterSet>("process");
+  /*fwlite::InputSource inputHandler_(process); fwlite::OutputFiles outputHandler_(process);
+  
+  // now get each parameter
+  const edm::ParameterSet& ana = process.getParameter<edm::ParameterSet>("electronAnalyzer");
+  edm::InputTag electrons_( ana.getParameter<edm::InputTag>("electrons") );
+  */
+  // setup an ID for use
+  const edm::ParameterSet& my_ids = process.getParameterSet("my_vid_configuration");
+  const edm::ParameterSet& loose_id_conf = my_ids.getParameterSet("loose");
+  const edm::ParameterSet& medium_id_conf = my_ids.getParameterSet("medium");
+  const edm::ParameterSet& tight_id_conf = my_ids.getParameterSet("tight");
+    
+  VersionedGsfElectronSelector loose_id(loose_id_conf);
+  VersionedGsfElectronSelector medium_id(medium_id_conf);
+  VersionedGsfElectronSelector tight_id(tight_id_conf); 
+
 
   const char* input_file_name = "root://cms-xrd-global.cern.ch//store/mc/RunIISpring15DR74/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/MINIAODSIM/Asympt50ns_MCRUN2_74_V9A-v2/00000/00C4781D-6B08-E511-8A0A-0025905A6084.root";
   TFile* file = TFile::Open(input_file_name );
@@ -22,19 +47,27 @@ int main(int argc, char* argv[])
     {
 
       // ID decisions objects
-      ElectronNtuplerVIDDemo demo(iConfig);
-	pat::ElectronCollection     electrons;
-
+      pat::ElectronCollection electrons;
       fwlite::Handle< pat::ElectronCollection > electronsHandle;
       electronsHandle.getByLabel(event, "slimmedElectrons");
       if(electronsHandle.isValid()){ electrons = *electronsHandle;}
-
+      unsigned int ind = 0;
       for (pat::ElectronCollection::iterator it = electrons.begin(); it != electrons.end(); it ++)
 	{
 	  //make ID decision for each elecron
+	  if( loose_id(*it, event) ) {
+	    std::cout << "Electron at " << ind << " passed loose ID!" << std::endl;
+          }
+          if( medium_id(*it, event) ) {
+	    std::cout << "Electron at " << ind << " passed medium ID!" << std::endl;
+          }
+          if( tight_id(*it,event) ) {
+	    std::cout << "Electron at " << ind << " passed tight ID!" << std::endl;
+          }
+	  ind ++;
 	}
       printf("read %lu\n", event_number);
-      getchar();
+      //      getchar();
       event_number ++;
       
     }
--- data/config_VID_test.py
+++ data/config_VID_test.py
@@ -1,85 +1,37 @@
-
-
 import FWCore.ParameterSet.Config as cms
-process = cms.Process("AnalysisProc")
-process.load("FWCore.MessageService.MessageLogger_cfi")
-process.MessageLogger.cerr.FwkReport.reportEvery = 10000
-import PhysicsTools.PythonAnalysis.LumiList as LumiList
-LumiList.LumiList().getVLuminosityBlockRange()
-process.source = cms.Source("PoolSource", fileNames = cms.untracked.vstring('') )
-from RecoJets.JetProducers.PileupJetIDParams_cfi import cutbased as pu_jetid
-#from UserCode.llvv_fwk.mvaConfig_cfi import ewkzp2jFullNoQG as mySignalMVA
-from CERN_RTU.TauAnalysis.mvaConfig_cfi import ewkzp2jFull as mySignalMVA
-#from UserCode.llvv_fwk.mvaConfig_cfi import ewkzp2jBase as mySignalMVA
 
-#
-# Set up electron ID (VID framework)
-#
-useAOD = False
-from PhysicsTools.SelectorUtils.tools.vid_id_tools import *
-# turn on VID producer, indicate data format  to be
-# DataFormat.AOD or DataFormat.MiniAOD, as appropriate 
-if useAOD == True :
-    dataFormat = DataFormat.AOD
-else :
-    dataFormat = DataFormat.MiniAOD
+process = cms.PSet()
 
-switchOnVIDElectronIdProducer(process, dataFormat)
-
-# define which IDs we want to produce
-my_id_modules = ['RecoEgamma.ElectronIdentification.Identification.cutBasedElectronID_Spring15_50ns_V2_cff',
-                 'RecoEgamma.ElectronIdentification.Identification.heepElectronID_HEEPV60_cff']
-
-#add them to the VID producer
-for idmod in my_id_modules:
-    setupAllVIDIdsInModule(process,idmod,setupVIDElectronSelection)
-
-process.ntupler = cms.EDAnalyzer(
-    'ElectronNtuplerVIDDemo',
-    # The module automatically detects AOD vs miniAOD, so we configure both
-    #
-    # Common to all formats objects
-    #
-    beamSpot = cms.InputTag('offlineBeamSpot'),
-    #
-    # Objects specific to AOD format
-    #
-    electrons    = cms.InputTag("gedGsfElectrons"),
-    genParticles = cms.InputTag("genParticles"),
-    vertices     = cms.InputTag("offlinePrimaryVertices"),
-    conversions  = cms.InputTag('allConversions'),
-    #
-    # Objects specific to MiniAOD format
-    #
-    electronsMiniAOD    = cms.InputTag("slimmedElectrons"),
-    genParticlesMiniAOD = cms.InputTag("prunedGenParticles"),
-    verticesMiniAOD     = cms.InputTag("offlineSlimmedPrimaryVertices"),
-    conversionsMiniAOD  = cms.InputTag('reducedEgamma:reducedConversions'),
-    #
-    # ID decisions (common to all formats)
-    #
-    eleVetoIdMap = cms.InputTag("egmGsfElectronIDs:cutBasedElectronID-Spring15-50ns-V2-standalone-veto"),
-    eleLooseIdMap = cms.InputTag("egmGsfElectronIDs:cutBasedElectronID-Spring15-50ns-V2-standalone-loose"),
-    eleMediumIdMap = cms.InputTag("egmGsfElectronIDs:cutBasedElectronID-Spring15-50ns-V2-standalone-medium"),
-    eleTightIdMap = cms.InputTag("egmGsfElectronIDs:cutBasedElectronID-Spring15-50ns-V2-standalone-tight"),
-    eleHEEPIdMap = cms.InputTag("egmGsfElectronIDs:heepElectronID-HEEPV60"),
-    # An example of configuration for accessing the full cut flow info for
-    # one case is shown below.
-    # The map name for the full info is the same as the map name of the
-    # corresponding simple pass/fail map above, they are distinguished by
-    # the type of the content.
-    eleMediumIdFullInfoMap = cms.InputTag("egmGsfElectronIDs:cutBasedElectronID-Spring15-50ns-V2-standalone-medium"),
-    # This is a fairly verbose mode if switched on, with full cut flow 
-    # diagnostics for each candidate. Use it in a low event count test job.
-    eleIdVerbose = cms.bool(False)
+process.fwliteInput = cms.PSet(
+    fileNames   = cms.vstring(['root://cms-xrd-global.cern.ch//store/data/Run2015D/DoubleEG/MINIAOD/PromptReco-v3/000/256/677/00000/9C593CF1-865F-E511-9198-02163E0135A8.root']),  ## mandatory
+    maxEvents   = cms.int32(-1),
+    outputEvery = cms.uint32(10)
     )
-outputFile = "VID_test_output.root"
-process.TFileService = cms.Service("TFileService",
-                                   fileName = cms.string( outputFile )
-                                   )
 
-# Make sure to add the ID sequence upstream from the user analysis module
-process.p = cms.Path(process.egmGsfElectronIDSequence * process.ntupler)
+process.fwliteOutput = cms.PSet(
+    fileName  = cms.string('fwlite_vid_example.root') ## mandatory
+    )
+
+process.electronAnalyzer = cms.PSet(
+    electrons = cms.InputTag("slimmedElectrons")
+    )
+
+#an example for cut based IDs
+from RecoEgamma.ElectronIdentification.Identification.cutBasedElectronID_Spring15_25ns_V1_cff import *
+
+if hasattr(cutBasedElectronID_Spring15_25ns_V1_standalone_loose,'isPOGApproved'):
+    del cutBasedElectronID_Spring15_25ns_V1_standalone_loose.isPOGApproved
+if hasattr(cutBasedElectronID_Spring15_25ns_V1_standalone_medium,'isPOGApproved'):
+    del cutBasedElectronID_Spring15_25ns_V1_standalone_medium.isPOGApproved
+if hasattr(cutBasedElectronID_Spring15_25ns_V1_standalone_tight,'isPOGApproved'):
+    del cutBasedElectronID_Spring15_25ns_V1_standalone_tight.isPOGApproved
+
+
+process.my_vid_configuration = cms.PSet(
+    loose = cutBasedElectronID_Spring15_25ns_V1_standalone_loose,
+    medium = cutBasedElectronID_Spring15_25ns_V1_standalone_medium,
+    tight = cutBasedElectronID_Spring15_25ns_V1_standalone_tight
+)
 
 PhysicsAnalysisParSet = cms.PSet(
     dtag           = cms.string("@dtag"),
